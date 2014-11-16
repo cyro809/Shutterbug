80ff6bdfdd9cd8cf1eac509d39595fac12adaf43
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2014-11-15 22:22:37 -0200

    Fazendo download da foto

--- Shutterbug/ImagesPlacesViewController.m
+++ Shutterbug/ImagesPlacesViewController.m
@@ -8,6 +8,7 @@
 
 #import "ImagesPlacesViewController.h"
 #import "FlickrFetcher.h"
+#import "ImageViewController.h"
 
 @interface ImagesPlacesViewController ()
 
@@ -45,7 +46,7 @@
     
     [self.refreshControl beginRefreshing]; // start the spinner
     NSLog(@"%@", self.place);
-    NSURL *url = [FlickrFetcher URLforPhotosInPlace:[self.place valueForKeyPath:FLICKR_PLACE_ID] maxResults:10];
+    NSURL *url = [FlickrFetcher URLforPhotosInPlace:[self.place valueForKeyPath:FLICKR_PLACE_ID] maxResults:50];
     // create a (non-main) queue to do fetch on
     dispatch_queue_t fetchQ = dispatch_queue_create("flickr fetcher", NULL);
     // put a block to do the fetch onto that queue
@@ -84,6 +85,50 @@
     cell.detailTextLabel.text = [photo valueForKeyPath:FLICKR_PHOTO_DESCRIPTION];
     
     return cell;
+}
+
+- (void)prepareImageViewController:(ImageViewController *)ivc toDisplayPhoto:(NSDictionary *)photo
+{
+    ivc.imageURL = [FlickrFetcher URLforPhoto:photo format:FlickrPhotoFormatLarge];
+    ivc.title = [photo valueForKeyPath:[FlickrFetcher extractNameOfPlace:[photo valueForKeyPath:FLICKR_PLACE_ID] fromPlaceInformation:photo]];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    
+    if ([sender isKindOfClass:[UITableViewCell class]]) {
+        // find out which row in which section we're seguing from
+        NSIndexPath *indexPath = [self.tableView indexPathForCell:sender];
+        if (indexPath) {
+            // found it ... are we doing the Display Photo segue?
+            if ([segue.identifier isEqualToString:@"Display Photo"]) {
+                // yes ... is the destination an ImageViewController?
+                if ([segue.destinationViewController isKindOfClass:[ImageViewController class]]) {
+                    // yes ... then we know how to prepare for that segue!
+                    [self prepareImageViewController:segue.destinationViewController
+                                      toDisplayPhoto:self.photos[indexPath.row]];
+                }
+            }
+        }
+    }
+}
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // get the Detail view controller in our UISplitViewController (nil if not in one)
+    id detail = self.splitViewController.viewControllers[1];
+    // if Detail is a UINavigationController, look at its root view controller to find it
+    if ([detail isKindOfClass:[UINavigationController class]]) {
+        detail = [((UINavigationController *)detail).viewControllers firstObject];
+    }
+    // is the Detail is an ImageViewController?
+    if ([detail isKindOfClass:[ImageViewController class]]) {
+        // yes ... we know how to update that!
+        [self prepareImageViewController:detail toDisplayPhoto:self.photos[indexPath.row]];
+    }
 }
 
 @end
